bbfbdb1a322c5988c8ac25500d1b4c19d22bb87b
Pedro <Continuum360@live.com>
2020-05-08 12:02:36 -0400

    Tensoflow load

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import flask
-from textblob import textblob
+import tensorflow as tf
 
-print("Hello World");
+# first, create a TensorFlow constant
+const = tf.constant(2.0, name="const")
+    
+# create TensorFlow variables
+b = tf.Variable(2.0, name='b')
+c = tf.Variable(1.0, name='c')
